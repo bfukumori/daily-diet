e0d2622257f9b5529ea4fceb4def56608a8b7744
Bruno Fukumori <brunofukumori@gmail.com>
2022-10-02 00:37:27 -0300

    add fonts, theme, remove boilerplate

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Text>Hello World</Text>
+    </Container>
+  );
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    white: '#FFFFFF',
+
+    'gray-1': '#1B1D1E',
+    'gray-2': '#333638',
+    'gray-3': '#5C6265',
+    'gray-4': '#B9BBBC',
+    'gray-5': '#DDDEDF',
+    'gray-6': '#EFF0F0',
+    'gray-7': '#FAFAFA',
+
+    'green-light': '#E5F0DB',
+    'green-mid': '#CBE4B4',
+    'green-dark': '#639339',
+
+    'red-light': '#F4E6E7',
+    'red-mid': '#F3BABD',
+    'red-dark': '#BF3B44',
+  },
+
+  FONT_FAMILY: {
+    regular: 'Nunito_400Regular',
+    bold: 'Nunito_700Bold',
+  },
+
+  FONT_SIZE: {
+    SM: '12px',
+    MD: '14px',
+    LG: '16px',
+  },
+};
